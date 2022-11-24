37fd898a20834be323fb8038669ced6227fd6367
fanfan <fanyu0819@163.com>
2022-11-24 15:45:21 +0800

    添加依赖

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,41 @@ import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import axios from "axios";
+import { ElMessage } from "element-plus";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
+
+axios.defaults.baseURL = "";
+app.config.globalProperties.$http = axios;
+
+//添加请求拦截器
+axios.interceptors.request.use(
+  function (config) {
+    //或许添加token
+    // 在发送请求之前做些什么
+    return config;
+  },
+  function (error) {
+    ElMessage.error(`请求失败：${error}`);
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
+// 添加响应拦截器
+axios.interceptors.response.use(
+  function (response) {
+    // 对响应数据做点什么
+    return response.data.res;
+  },
+  function (error) {
+    ElMessage.error(`请求失败：${error}`);
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  }
+);
 
 app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,10 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-// import HomeView from "../views/HomeView.vue";
 
-const routes: Array<RouteRecordRaw> = [
-  // {
-  //   path: "/",
-  //   name: "home",
-  //   component: HomeView,
-  // },
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  // },
-];
+const routes: Array<RouteRecordRaw> = [];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 });
 
 export default router;
