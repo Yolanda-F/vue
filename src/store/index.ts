3b71a69651777f8ace3bcb5886b851acd4aaf2db
fanfan <fanyu0819@163.com>
2022-11-28 13:03:41 +0800

    pinia部分持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
 import { defineStore } from "pinia";
 
 export const useStore = defineStore("index", {
+  //持久化存储，默认localstorage
   persist: {
     enabled: true,
+    strategies: [
+      {
+        key: "userName",
+        paths: ["userName"],
+      },
+    ],
   },
   state: () => {
     return {
       userName: "", //用户名
+      roles: [], //用户角色
+      count: 1,
     };
   },
   actions: {},
